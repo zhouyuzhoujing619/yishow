9ee240d3fe934e938d5d7b82e89647e502f68605
zhouyuzhoujing619 <1650986994@qq.com>
2016-08-20 13:47:58 +0800

    ‘基于ecshop开发的电子商城

---
+++ data/flashdata/redfocus/data.js
@@ -0,0 +1,13 @@
+imgUrl1="data/afficheimg/20160804ogpaxc.jpg";
+imgtext1="";
+imgLink1=escape("http://");
+imgUrl2="data/afficheimg/20160804ldnjmn.jpg";
+imgtext2="";
+imgLink2=escape("http://");
+imgUrl3="data/afficheimg/20160804trhhgy.jpg";
+imgtext3="";
+imgLink3=escape("http://");
+
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3;
